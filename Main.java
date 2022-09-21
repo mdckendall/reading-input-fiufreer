bfb9897bfc4f9f6b033504468f948241c8b9ae5b
fiufreer <114039460+fiufreer@users.noreply.github.com>
2022-09-20 21:39:12 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
+    Scanner s = new Scanner(System.in);
+    String w = s.next();
+    System.out.println(w.length());
     //Your code will return the length of the entered String
   }
 }
